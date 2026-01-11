5b8bed0908c4262e17cd5d9bfc72a3e889a0ee25
tigmen <tigmenal@gmail.com>
2026-01-12 02:25:17 +0300

    log message id

--- internal/telegram/telegram.go
+++ internal/telegram/telegram.go
@@ -49,6 +49,8 @@ func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
 		"Telegram message",
 		log.String("From", update.Message.From.Username),
 		log.String("Text", update.Message.Text),
+		log.Int("Thread ID", update.ChannelPost.MessageThreadID),
+		log.Int("Message ID", update.Message.ID),
 	)
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
