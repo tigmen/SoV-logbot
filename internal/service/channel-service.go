5dd5f0dce9bdf430b1e1c7d782de9f89147a92e0
tigmen <tigmenal@gmail.com>
2026-01-13 14:18:07 +0300

    qfix

--- internal/service/channel-service.go
+++ internal/service/channel-service.go
@@ -62,13 +62,14 @@ func (s *Service) UpdateChannel(ctx context.Context, guildID string, updates map
 						return err
 					}
 				} else {
+					delete(s.message, key)
+
 					_, err := s.bot.DeleteMessage(
 						ctx, gid, s.message[key],
 					)
 					if err != nil {
 						return err
 					}
-
 				}
 			}
 		} else {
